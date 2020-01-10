97225153f5398f7842d1a56278fd04af78df9a07
Catalin Patulea <catalinp@google.com>
2020-01-09 22:35:32 -0500

    Collect nl80211, nf_conntrack.

--- mat/prometheus/collect.c
+++ mat/prometheus/collect.c
@@ -1,13 +1,21 @@
+#include <arpa/inet.h>
+#include <asm/types.h>
 #include <assert.h>
 #include <dirent.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <limits.h>
+#include <linux/genetlink.h>
+#include <linux/if.h>
+#include <linux/netlink.h>
+#include <linux/nl80211.h>
 #include <signal.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/time.h>
 #include <sys/types.h>
@@ -18,6 +26,8 @@
   _Generic((x), \
     int: "%d", \
     unsigned int: "%u", \
+    short int: "%hd", \
+    short unsigned int: "%hu", \
     long int: "%ld", \
     long long int: "%lld", \
     void *: "%p" \
@@ -89,10 +99,60 @@ off_t file_size = 1024 * 1024;
 off_t max_total = 10 * 1024 * 1024;
 int period = 2;
 
+void tar_write(FILE *f, const char *t, const char *dir, const char *name,
+               void *data, size_t size) {
+  struct tar h = {0};
+  CHECK_LT(strlen(name), sizeof(h.path));
+  strcpy(h.path, name);
+  strcpy(h.mode, "0400");
+  strcpy(h.uid, "0");
+  strcpy(h.gid, "0");
+  sprintf(h.size, "%011o", size);
+  sprintf(h.mtime, "%011lo", time(NULL));
+  strncpy(h.checksum, "        ", sizeof(h.checksum));
+  strncpy(h.link, "0", sizeof(h.link));
+  strncpy(h.ustar, "ustar", sizeof(h.ustar));
+  strncpy(h.ver, "00", sizeof(h.ver));
+  bprintf(h.prefix, "%s%s", t, dir);
+
+  const unsigned char *p = (const unsigned char *)&h;
+  int cs = 0;
+  for (size_t i = 0; i < sizeof(h); ++i) {
+    cs += p[i];
+  }
+  bprintf(h.checksum, "%06o", cs);
+
+  CHECK_EQ(1u, fwrite(&h, sizeof(h), 1, f));
+  CHECK_EQ(1u, fwrite(data, size, 1, f));
+  if (size % 512) {
+    int pad = 512 - (size % 512);
+    char zero[512] = {0};
+    CHECK_EQ(1u, fwrite(zero, pad, 1, f));
+  }
+}
+
 bool in_exclude(const char *name, const char *exclude) {
   char xname[100];
   bprintf(xname, " %s ", name);
   return strstr(exclude, xname);
+}
+
+void collect_file(FILE *f, const char *t, const char *dir, const char *name) {
+  char path[100];
+  bprintf(path, "%s/%s", dir, name);
+
+  FILE *file = fopen(path, "r");
+  // May have raced, file no longer exists.
+  if (file) {
+    char data[32768];
+    int rc = fread(data, 1, sizeof(data), file);
+    CHECK_GE(rc, 0);
+
+    // Ignore ferror.
+    CHECK_EQ(0, fclose(file));
+
+    tar_write(f, t, dir, name, data, rc);
+  }
 }
 
 void collect(FILE *f, const char *t, const char *dir, const char *exclude) {
@@ -105,54 +165,13 @@ void collect(FILE *f, const char *t, const char *dir, const char *exclude) {
   struct dirent *ent;
   while ((ent = readdir(d))) {
     if (ent->d_type == DT_REG && !in_exclude(ent->d_name, exclude)) {
-      char path[100];
-      bprintf(path, "%s/%s", dir, ent->d_name);
-
-      FILE *file = fopen(path, "r");
-      // May have raced, file no longer exists.
-      if (file) {
-        char data[4096];
-        int rc = fread(data, 1, sizeof(data), file);
-        CHECK_GE(rc, 0);
-
-        // Ignore ferror.
-        CHECK_EQ(0, fclose(file));
-
-        struct tar h = {0};
-        CHECK_LT(strlen(ent->d_name), sizeof(h.path));
-        strcpy(h.path, ent->d_name);
-        strcpy(h.mode, "0400");
-        strcpy(h.uid, "0");
-        strcpy(h.gid, "0");
-        sprintf(h.size, "%011o", rc);
-        sprintf(h.mtime, "%011lo", time(NULL));
-        strncpy(h.checksum, "        ", sizeof(h.checksum));
-        strncpy(h.link, "0", sizeof(h.link));
-        strncpy(h.ustar, "ustar", sizeof(h.ustar));
-        strncpy(h.ver, "00", sizeof(h.ver));
-        bprintf(h.prefix, "%s%s", t, dir);
-
-        const unsigned char *p = (const unsigned char *)&h;
-        int cs = 0;
-        for (size_t i = 0; i < sizeof(h); ++i) {
-          cs += p[i];
-        }
-        bprintf(h.checksum, "%06o", cs);
-
-        CHECK_EQ(1u, fwrite(&h, sizeof(h), 1, f));
-        CHECK_EQ(1u, fwrite(data, rc, 1, f));
-        if (rc % 512) {
-          int pad = 512 - (rc % 512);
-          memset(data, 0, pad);
-          CHECK_EQ(1u, fwrite(data, pad, 1, f));
-        }
-      }
+      collect_file(f, t, dir, ent->d_name);
     }
   }
   PCHECK_EQ(0, closedir(d));
 }
 
-void cycle(FILE *f, const char *t) {
+void cycle_debugfs(FILE *f, const char *t) {
   const char dir[] = "/sys/kernel/debug/ieee80211";
   DIR *d = opendir(dir);
   PCHECK_NE(NULL, (void *)d);
@@ -199,6 +218,137 @@ void cycle(FILE *f, const char *t) {
   }
 
   PCHECK_EQ(0, closedir(d));
+}
+
+int nlfd;
+short nlfamily;
+
+void nl_getfamily() {
+  {
+    struct {
+      struct nlmsghdr nh;
+      struct genlmsghdr gm;
+      struct nlattr a;
+      char buf[sizeof(NL80211_GENL_NAME)];
+    } gf;
+    gf.nh.nlmsg_len = sizeof(gf);
+    gf.nh.nlmsg_type = GENL_ID_CTRL;
+    gf.nh.nlmsg_flags = NLM_F_REQUEST;
+    gf.nh.nlmsg_seq = 1;
+    gf.nh.nlmsg_pid = 0;
+    gf.gm.cmd = CTRL_CMD_GETFAMILY;
+    gf.gm.version = 0;
+    gf.gm.reserved = 0;
+    strcpy(gf.buf, NL80211_GENL_NAME);
+    gf.a.nla_type = CTRL_ATTR_FAMILY_NAME;
+    gf.a.nla_len = sizeof(gf.a) + strlen(gf.buf) + 1;
+
+    struct sockaddr_nl sa = {.nl_family = AF_NETLINK};
+    struct iovec iov = {&gf, sizeof(gf)};
+    struct msghdr msg = {&sa, sizeof(sa), &iov, 1, NULL, 0, 0};
+
+    ssize_t rc = sendmsg(nlfd, &msg, 0);
+    PCHECK_EQ((ssize_t)iov.iov_len, rc);
+  }
+
+  {
+    struct {
+      struct nlmsghdr nh;
+      struct genlmsghdr gm;
+      char buf[2048];
+    } b;
+    struct iovec iov = {&b, sizeof(b)};
+    struct msghdr msg = {NULL, 0, &iov, 1, NULL, 0, 0};
+    ssize_t rc = recvmsg(nlfd, &msg, 0);
+    PCHECK_GE(rc, 0);
+    CHECK_EQ(0, msg.msg_flags & MSG_TRUNC);
+    CHECK_GE(rc, (ssize_t)b.nh.nlmsg_len);
+    CHECK_EQ(GENL_ID_CTRL, b.nh.nlmsg_type);
+    CHECK_EQ(0, b.nh.nlmsg_flags & NLM_F_MULTI);
+    CHECK_EQ(1u, b.nh.nlmsg_seq);
+
+    nlfamily = 0;
+
+    struct nlattr *a = (struct nlattr *)&b.buf;
+    while ((char *)a < (char *)&b + b.nh.nlmsg_len) {
+      if (a->nla_type == CTRL_ATTR_FAMILY_ID) {
+        nlfamily = *(short *)(a + 1);
+        break;
+      }
+      a = (struct nlattr *)((char *)a + NLA_ALIGN(a->nla_len));
+    }
+
+    CHECK_NE(0, nlfamily);
+    fprintf(stderr, "nl80211 family id: %hd\n", nlfamily);
+  }
+}
+
+int getif(const char *name) {
+  int fdp = socket(AF_UNIX, SOCK_DGRAM, 0);
+  PCHECK_GE(fdp, 0);
+  struct ifreq ifr;
+  CHECK_LT(strlen(name), sizeof(ifr.ifr_name));
+  strcpy(ifr.ifr_name, name);
+  PCHECK_GE(ioctl(fdp, SIOCGIFINDEX, &ifr), 0);
+  PCHECK_GE(close(fdp), 0);
+  return ifr.ifr_ifindex;
+}
+
+void nl_dump_stations(FILE *f, const char *t, const char *ifname, int ifindex) {
+  char dir[100];
+  bprintf(dir, "/nl80211/%s/stations", ifname);
+
+  {
+    struct {
+      struct nlmsghdr nh;
+      struct genlmsghdr gm;
+      struct nlattr a;
+      int ifindex;
+    } gf;
+    gf.nh.nlmsg_len = sizeof(gf);
+    gf.nh.nlmsg_type = nlfamily;
+    gf.nh.nlmsg_flags = NLM_F_REQUEST | NLM_F_DUMP;
+    gf.nh.nlmsg_seq = 2;
+    gf.nh.nlmsg_pid = 0;
+    gf.gm.cmd = NL80211_CMD_GET_STATION;
+    gf.gm.version = 0;
+    gf.gm.reserved = 0;
+    gf.a.nla_type = NL80211_ATTR_IFINDEX;
+    gf.a.nla_len = sizeof(gf.a) + sizeof(gf.ifindex);
+    gf.ifindex = ifindex;
+
+    struct sockaddr_nl sa = {.nl_family = AF_NETLINK};
+    struct iovec iov = {&gf, sizeof(gf)};
+    struct msghdr msg = {&sa, sizeof(sa), &iov, 1, NULL, 0, 0};
+
+    ssize_t rc = sendmsg(nlfd, &msg, 0);
+    PCHECK_EQ((ssize_t)iov.iov_len, rc);
+  }
+
+  for (int multi = 0;; ++multi) {
+    struct {
+      struct nlmsghdr nh;
+      struct genlmsghdr gm;
+      char buf[20480];
+    } b;
+    struct iovec iov = {&b, sizeof(b)};
+    struct msghdr msg = {NULL, 0, &iov, 1, NULL, 0, 0};
+    ssize_t rc = recvmsg(nlfd, &msg, 0);
+    PCHECK_GE(rc, 0);
+    CHECK_EQ(0, msg.msg_flags & MSG_TRUNC);
+    CHECK_GE(rc, (ssize_t)b.nh.nlmsg_len);
+    CHECK_NE(0, b.nh.nlmsg_flags & NLM_F_MULTI);
+    CHECK_EQ(2u, b.nh.nlmsg_seq);
+    if (b.nh.nlmsg_type == NLMSG_DONE) {
+      break;
+    }
+    CHECK_EQ(nlfamily, b.nh.nlmsg_type);
+
+    fprintf(stderr, "stations got %d bytes\n", rc);
+    char name[10];
+    bprintf(name, "%04d", multi);
+    tar_write(f, t, dir, name, &b, rc);
+  }
 }
 
 bool strsuffix(const char *haystack, const char *needle) {
@@ -266,7 +416,13 @@ void collectloop() {
     bool full;
     do {
       bprintf(t, "%09ld%06ld", tv.tv_sec, tv.tv_usec);
-      cycle(f, t);
+      cycle_debugfs(f, t);
+
+      collect_file(f, t, "/proc/net", "nf_conntrack");
+
+      int ifindex = getif("wlan0");
+      CHECK_NE(0, ifindex);
+      nl_dump_stations(f, t, "wlan0", ifindex);
 
       PCHECK_EQ(0, fflush(f));
 
@@ -330,6 +486,11 @@ int main(int argc, char **argv) {
       return 1;
     }
   }
+
+  nlfd = socket(AF_NETLINK, SOCK_RAW, NETLINK_GENERIC);
+  PCHECK_GE(nlfd, 0);
+
+  nl_getfamily();
 
   sigset_t set;
   PCHECK_EQ(0, sigemptyset(&set));
